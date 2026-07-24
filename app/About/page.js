6ad5789635afadb61bdc5451154890186b44a38d
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-24 22:13:07 +0300

    Added new styling for the technology being used

--- app/About/page.js
+++ app/About/page.js
@@ -5,8 +5,16 @@ export default function About(){
         <div className="container mx-auto">
 <div className="flex flex-col">
     <span className="text-teal-500 text-sm">Hi, my name is</span>
+  <div className="text-4xl p-2 ">
+    Mary Nasieku
+  </div>
+<div className="p-3">
+  I'm a passionate Full-Stack Software Developer who enjoys building
+  modern, responsive, and scalable web applications. I specialize in <span className="text-teal-500"><em >React</em></span>,
+<span className="text-teal-500"><em> Next.js,</em></span> <span className="text-teal-500"> <em >Tailwind CSS,</em></span> <span className="text-teal-500"><em >JavaScript,</em> </span>and backend technologies, creating
+seamless, user-centered digital experiences from concept to deployment.
+</div>
 
-    
 </div>
 </div>
     )

229839091c655b9c52e9552e20026f50721500e5
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-24 21:54:00 +0300

    Added a conatiner on about page and styling

--- app/About/page.js
+++ app/About/page.js
@@ -2,8 +2,12 @@
 "use client"
 export default function About(){
     return(
-<div>
+        <div className="container mx-auto">
+<div className="flex flex-col">
+    <span className="text-teal-500 text-sm">Hi, my name is</span>
+
     
+</div>
 </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client"
 import Link
- from "next/link"
+from "next/link"
 import Nav from "./nav/page"
+import About from "./About/page"
 export default function Home(){
   return(
     <>
 <Nav/>
 <main>
-
+<About/>
 </main>
     </>
   )

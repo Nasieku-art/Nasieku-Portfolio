dea56293c7f1e3edadd174be36b79da6d8771f17
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-24 15:36:52 +0300

    Added a new styling

--- app/page.js
+++ app/page.js
@@ -2,19 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-    <main>
-<h1 className="flex justify-center  text-teal-600  text-3xl font-serif ">
+    <div className="container mx-auto flex justify-start">
+    <div className="flex ">
+<h1 className=" justify-center  text-teal-600  font-serif ">
   <div>
  <Image
- src="/image/MN Logo.svg" 
+ src="/image/Nasieku Logo.png" 
         alt="Mary Nasieku" 
-        width={500} 
-        height={300}
+        width={200} 
+        height={100}
  />
  </div>
+ <div className="text-amber-50 flex items-center gap-5">
+      <div >
+        <button>
+          About
+        </button>
+      </div>
+<div>
+  <button>
+    Projects
+  </button>
+</div>
+<div>
+  <button>
+    Experience
+  </button>
+</div>
+<div>
+  <button>
+    Contact
+  </button>
+</div>
+<div>
+  <button>
+    Resume
+  </button>
+</div>
+    </div>
 </h1>
-    </main>
+    </div>
+    
     </div>
   );
 }

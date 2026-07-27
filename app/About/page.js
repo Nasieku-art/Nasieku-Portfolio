649a0b1508ed6fe8460f6919371bf185c340f27c
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-27 11:02:26 +0300

    Added a new section for technologies that i am currently using to create my projects

--- app/About/page.js
+++ app/About/page.js
@@ -1,41 +1,78 @@
-"use client"
+"use client";
 import Image from "next/image";
 
 export default function About() {
   return (
     <div className="container mx-auto" id="about">
       <div className="flex flex-col md:flex-row items-center gap-8">
-
-        <div className="py-10 md:py-55 flex-1">
+        <div className="py-3 md:py-55 flex-1">
           <h1 className="font-bold text-3xl">About Me</h1>
           <div className="py-3">
             <p>
-              I'm a passionate <span className="text-teal-500">Full-Stack Software Developer </span>who enjoys
-              transforming
+              I'm a passionate{" "}
+              <span className="text-teal-500">
+                Full-Stack Software Developer{" "}
+              </span>
+              who enjoys transforming
             </p>
-            <p>ideas into modern, scalable, and high-performing web applications. I</p>
-            <p>specialize in building seamless digital experiences using technologies such as</p>
             <p>
-              <span className="text-teal-500">React, Next.js, Tailwind CSS, JavaScript, Node.js, </span>and databases,
-              combining
+              ideas into modern, scalable, and high-performing web applications.
+              I
+            </p>
+            <p>
+              specialize in building seamless digital experiences using
+              technologies such as
+            </p>
+            <p>
+              <span className="text-teal-500">
+                React, Next.js, Tailwind CSS, JavaScript, Node.js,{" "}
+              </span>
+              and databases, combining
             </p>
             <p>intuitive user interfaces with robust backend systems.</p>
           </div>
 
           <div className="py-3">
-            <p>My journey in technology extends beyond software development. I have</p>
-            <p>experience in IT support, digital skills training, and community mentorship,</p>
-            <p>where I've helped learners develop practical technical skills. These</p>
-            <p>experiences have strengthened my communication, leadership, and problem-</p>
-            <p>solving abilities while reinforcing my belief that technology should empower</p>
+            <p>
+              My journey in technology extends beyond software development. I
+              have
+            </p>
+            <p>
+              experience in IT support, digital skills training, and community
+              mentorship,
+            </p>
+            <p>
+              where I've helped learners develop practical technical skills.
+              These
+            </p>
+            <p>
+              experiences have strengthened my communication, leadership, and
+              problem-
+            </p>
+            <p>
+              solving abilities while reinforcing my belief that technology
+              should empower
+            </p>
             <p>people and create opportunities.</p>
           </div>
 
           <div className="py-3">
-            <p>I'm constantly exploring emerging technologies, particularly artificial</p>
-            <p>intelligence, cloud technologies, and modern web development, to build</p>
-            <p>innovative solutions that address real-world challenges. Every project I</p>
-            <p>undertake is an opportunity to learn, innovate, and deliver secure, scalable,</p>
+            <p>
+              I'm constantly exploring emerging technologies, particularly
+              artificial
+            </p>
+            <p>
+              intelligence, cloud technologies, and modern web development, to
+              build
+            </p>
+            <p>
+              innovative solutions that address real-world challenges. Every
+              project I
+            </p>
+            <p>
+              undertake is an opportunity to learn, innovate, and deliver
+              secure, scalable,
+            </p>
             <p>and meaningful digital experiences.</p>
           </div>
         </div>
@@ -44,13 +81,19 @@ export default function About() {
           <Image
             src="/image/Mary Nasieku Photo.jpg"
             alt="Mary Nasieku"
-            width={150}
+            width={200}
             height={50}
             loading="lazy"
-            className="rounded-lg object-cover"
+            className="rounded-lg object-cover "
           />
         </div>
-
+      </div>
+      <div className="py-3 flex text-center">
+        <p>Here are some of the technologies that I have been working with:</p>
+        <p>Javascript(ES6+)</p>
+        <p> React</p>
+        <p>Nextjs</p>
+        <p>WordPress</p>
       </div>
     </div>
   );

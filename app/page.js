0056c339b0213d8328aa646012bad34421884f97
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-08-04 20:50:32 +0300

    added new icons for social media

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import About from "./about/page";
 import Projects from "./projects/page";
 import Experience from "./experience/page";
 import Contact from "./contact/page";
+import Footer from "./footer/page";
+
 
 export default function Home() {
   return (
@@ -17,7 +19,9 @@ export default function Home() {
         <Projects />
         <Experience />
         <Contact/>
+        <Footer/>
       </main>
+    
     </div>
   );
 }

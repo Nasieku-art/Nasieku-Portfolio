9cafa1632b14d55078a1680c30367537a2b18133
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-08-18 11:34:02 +0300

    added a scroll-smooth effect on layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
